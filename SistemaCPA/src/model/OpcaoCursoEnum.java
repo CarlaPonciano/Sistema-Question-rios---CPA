9a9d06c99a6849bd977659fa9d58ee51b4f28330
CarlaPonciano <49000683+CarlaPonciano@users.noreply.github.com>
2019-10-22 15:21:53 -0300

    .

--- SistemaCPA/src/model/OpcaoCursoEnum.java
+++ SistemaCPA/src/model/OpcaoCursoEnum.java
@@ -10,5 +10,19 @@ package model;
  * @author carli
  */
 public enum OpcaoCursoEnum {
-    
+    SISTEMAS_DE_INFORMACAO(1), 
+    ENGENHARIA_METALURGICA(2), 
+    ADMINISTRACAO(3),
+    PEDAGOGIA(4);
+
+    private final int id;
+
+    OpcaoCursoEnum(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+      
 }
