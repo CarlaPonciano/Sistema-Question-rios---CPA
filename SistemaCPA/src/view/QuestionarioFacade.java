da94e9d29db2c73ab9ea39e0d7285fd8de337373
CarlaPonciano <49000683+CarlaPonciano@users.noreply.github.com>
2019-10-27 12:58:58 -0300

    cpa web

---
+++ SistemaCPA/src/view/QuestionarioFacade.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author carli
+ */
+public class QuestionarioFacade {
+    public boolean cadastrarQuestionario(){
+        return true;
+    }
+}
